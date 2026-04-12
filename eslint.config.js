59ad1f507a62f3d3aadb6bab6a6164f4e8ecb17a
rvcas <x@rvcas.dev>
2026-04-11 23:03:50 -0400

    chore: add plutus

--- eslint.config.js
+++ eslint.config.js
@@ -12,6 +12,7 @@ const gitignorePath = path.resolve(import.meta.dirname, ".gitignore");
 
 export default defineConfig(
   includeIgnoreFile(gitignorePath),
+  { ignores: ["worker-configuration.d.ts"] },
   js.configs.recommended,
   ts.configs.recommended,
   svelte.configs.recommended,
